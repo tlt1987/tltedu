7592fafdec8be5a3950c8711da32a5cdab3585c8
tlt <450215553@qq.com>
2018-04-04 23:03:57 +0800

    项目初始框架

---
+++ src/main/java/com/fongdong/tltedu/TlteduApplication.java
@@ -0,0 +1,12 @@
+package com.fongdong.tltedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlteduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlteduApplication.class, args);
+	}
+}
